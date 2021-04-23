bba17a1698dae4902436e75ea61b0073d8e3a989
Max Klenk <max@klenk.biz>
2021-04-23 22:23:23 +0200

    cleanup

--- packages/react-app/src/views/Trade.jsx
+++ packages/react-app/src/views/Trade.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Button, Input } from "antd";
-import { AddressInput } from "../components";
-import { utils, BigNumber, constants } from "ethers";
-import { parseEther } from "@ethersproject/units";
-import { Transactor } from "../helpers";
-import { useGasPrice } from "../hooks/index.js";
+import React from "react";
 
 export default function Trade(props) {
 
- 
   return (
     <div className="textPage">
         <h2>Trade</h2>
-        <div>
-          <h3>Coming soon</h3>
+        <div className="textBlock">
+          <h1>Coming soon</h1>
         </div>
     </div>
   );
