68850093ed01be46fb8b688245518531e1240a4d
Max Klenk <max@klenk.biz>
2021-04-23 17:14:00 +0200

    update legal pages

--- packages/react-app/src/views/Imprint.jsx
+++ packages/react-app/src/views/Imprint.jsx
@@ -5,6 +5,13 @@ export default function Imprint() {
   return (
     <div className="textPage">
         <h2>Imprint</h2>
+
+        <div className="textBlock">
+          STOMT GmbH<br/>
+          Charlottenstraße 116<br/>
+          14467 Potsdam<br/>
+          info@stomt.com<br/>
+        </div>
     </div>
   );
 }
